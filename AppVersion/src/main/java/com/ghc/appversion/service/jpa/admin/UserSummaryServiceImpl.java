38c04ea1fbb7d1f4b02c7e50aae865f78af7d0cb
chung.pv0795@gmail.com <chung.pv0795@gmail.com@91e55eb3-8985-b3cb-6bf5-e0d0f538df24>
2014-04-21 10:29:55 +0000

    - add UserSummary

--- AppVersion/src/main/java/com/ghc/appversion/service/jpa/admin/UserSummaryServiceImpl.java
+++ AppVersion/src/main/java/com/ghc/appversion/service/jpa/admin/UserSummaryServiceImpl.java
@@ -5,6 +5,7 @@
  */
 package com.ghc.appversion.service.jpa.admin;
 
+import java.util.Iterator;
 import java.util.List;
 
 import javax.persistence.EntityManager;
@@ -14,12 +15,14 @@ import javax.persistence.Query;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Order;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.ghc.appversion.domain.admin.UserSummary;
 import com.ghc.appversion.util.JpaUtil;
+import com.ghc.appversion.util.LogUtil;
 
 /**
  * 
@@ -36,7 +39,22 @@ public class UserSummaryServiceImpl implements UserSummaryService {
 	@Transactional(readOnly = true)
 	public Page<UserSummary> findAllByPage(Pageable pageable) {
 		Query query = entityManager
-				.createNativeQuery("SELECT distinct USER_ID, EMAIL, GROUP_CONCAT(GROUP_NAME) as GROUPNAMES FROM view_user_summary WHERE 1 group by USER_ID");
+				.createNativeQuery("SELECT distinct USER_ID, EMAIL, GROUP_CONCAT(GROUP_NAME) as GROUPNAMES FROM view_user_summary WHERE 1 group by USER_ID ORDER BY :orderBy :sort LIMIT :limit OFFSET :offset");
+		String orderBy = "";
+		String sort = "";
+		Iterator<Order> i = pageable.getSort().iterator();
+		while(i.hasNext()) {
+			Order order = i.next();
+			orderBy = order.getProperty();
+			sort = order.getDirection().name();
+		}
+		query.setParameter("orderBy", orderBy);
+		query.setParameter("sort", sort);
+		query.setParameter("limit", pageable.getPageSize());
+		query.setParameter("offset", pageable.getOffset());
+		
+		LogUtil.error("orderBy %s sort %s limit %s offset %s", orderBy, sort, pageable.getPageSize(), pageable.getOffset());
+		
 		List<UserSummary> result = JpaUtil.getResultList(query, UserSummary.class);
 		
 		Page<UserSummary> page = new PageImpl<>(result);
